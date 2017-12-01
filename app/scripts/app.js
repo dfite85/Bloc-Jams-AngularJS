6c5c2491c801bedff56c443ab7e45663dc0165ab
Dustin Fite <Dfite85@gmail.com>
2017-11-30 18:41:42 -0600

    revised lesson 4 assingment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,49 +1,21 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> ec7433774e6a0db2e51450dc35fa0c44c53ca5c7
 (function() {
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
                 enabled: true,
                 requireBase: false
-<<<<<<< HEAD
-            });
-        
-        $stateProvider
-            .state('landing', {
-                url: '/',
-                controller: 'LandingCtrl as landing',
-=======
             })
         $stateProvider
             .state('landing', {
                 url: '/',
->>>>>>> ec7433774e6a0db2e51450dc35fa0c44c53ca5c7
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
                 url: '/album',
-<<<<<<< HEAD
-                controller: 'AlbumCtrl as album',
-=======
->>>>>>> ec7433774e6a0db2e51450dc35fa0c44c53ca5c7
                 templateUrl: '/templates/album.html'
             })
             .state('collection', {
                 url: '/collection',
-<<<<<<< HEAD
-                controller: 'CollectionCtrl as collection',
-                templateUrl: '/templates/collection.html'
-            });
-    }
-
-    angular
-        .module('blocJams', ['ui.router'])
-        .config(config);
-})();
-=======
                 templateUrl: '/templates/collection.html'
             });
     }
@@ -52,4 +24,4 @@
         .module('blocJams', ['ui.router'])
         .config(config);
 })();
->>>>>>> ec7433774e6a0db2e51450dc35fa0c44c53ca5c7
+
